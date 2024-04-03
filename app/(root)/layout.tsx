36d9f09382ee8116968ffb03a76bae07121d515c
Vishal <vishalpal1501@gmail.com>
2024-04-03 15:55:41 +0530

    Routing done sidebae with section

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -3,7 +3,6 @@ import React, { ReactNode } from 'react';
 function RootLayout({ children }: { children: ReactNode }) {
   return (
     <main>
-      Navbar
       {children}
       Footer
     </main>
