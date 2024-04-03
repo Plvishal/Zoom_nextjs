a85059477926e6b5dd23662991192bbfdeccfb6b
Vishal <vishalpal1501@gmail.com>
2024-04-03 17:38:59 +0530

    layout design  done with responsive

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -56,6 +56,9 @@ const config = {
           1: '#1C1F2E',
           2: '#161925',
         },
+        blue: {
+          1: '#0E78F9',
+        },
       },
       borderRadius: {
         lg: 'var(--radius)',
