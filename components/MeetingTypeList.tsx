291d5ca645960d072ef831c34bb38fe0f18b96f4
Vishal <vishalpal1501@gmail.com>
2024-04-05 09:25:55 +0530

    Using stream we can set call with audio and video

--- components/MeetingTypeList.tsx
+++ components/MeetingTypeList.tsx
@@ -4,13 +4,61 @@ import React, { useState } from 'react';
 import HomeCart from './HomeCart';
 import { useRouter } from 'next/navigation';
 import MeetingModel from './MeetingModel';
+import { useUser } from '@clerk/nextjs';
+import { Call, useStreamVideoClient } from '@stream-io/video-react-sdk';
+import { useToast } from './ui/use-toast';
 
 function MeetingTypeList() {
   const router = useRouter();
   const [meetingState, setMeetingState] = useState<
     'isScheduleMeeting' | 'isJoiningMeeting' | 'isInstantMeeting' | undefined
   >();
-  const createMeeting = () => {};
+  const [values, setValues] = useState({
+    dateTime: new Date(),
+    description: '',
+    link: '',
+  });
+  const { toast } = useToast();
+  const [callDetails, setCallDetails] = useState<Call>();
+  const { user } = useUser();
+  const client = useStreamVideoClient();
+  const createMeeting = async () => {
+    if (!user || !client) return;
+    try {
+      if (!values.dateTime) {
+        toast({
+          title: 'Please select a date and time',
+        });
+        return;
+      }
+      const id = crypto.randomUUID();
+      const call = client.call('default', id);
+      if (!call) throw new Error('Failed to create call');
+      const startAt =
+        values.dateTime.toISOString() || new Date(Date.now()).toISOString();
+      const description = values.description || 'Instant meeting';
+      await call.getOrCreate({
+        data: {
+          starts_at: startAt,
+          custom: {
+            description,
+          },
+        },
+      });
+      setCallDetails(call);
+      if (!values.description) {
+        router.push(`/meeting/${call.id}`);
+      }
+      toast({
+        title: 'Meeting Created',
+      });
+    } catch (error) {
+      console.log(error);
+      toast({
+        title: 'Failed to create meeting',
+      });
+    }
+  };
   return (
     <section className="grid grid-cols-1 gap-5 md:grid-cols-2 xl:grid-cols-4">
       <HomeCart
