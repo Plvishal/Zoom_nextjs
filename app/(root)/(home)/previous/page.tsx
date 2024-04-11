5adae4cf58156210ce47a1ddf79f11554a61827b
Vishal <vishalpal1501@gmail.com>
2024-04-11 17:14:47 +0530

    previous meeting done

--- app/(root)/(home)/previous/page.tsx
+++ app/(root)/(home)/previous/page.tsx
@@ -1,9 +1,12 @@
+import CallList from '@/components/CallList';
 import React from 'react';
 
 function Previous() {
   return (
     <section className="flex size-full flex-col gap-10 text-white">
-      <h1 className="text-3xl font-bold">Previous</h1>
+      <h1 className="text-3xl font-bold">
+        <CallList type="ended" />
+      </h1>
     </section>
   );
 }
